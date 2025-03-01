0f166b94d94a410bd9bb4d2094a5205c9e47c3cb
StefanoGarzoni <stefano.garzoni@mail.polimi.it>
2025-03-01 15:12:00 +0100

    RAG

--- RAG/rag.py
+++ RAG/rag.py
@@ -5,9 +5,9 @@ os.chdir("code")
 try:
     result=subprocess.run(["python", "pdfToTxt.py"])
     if(result):
-        print("conversione da pdf a txt | eseguito con successo")
+        print("PDF to TXT conversion | executed successfully")
     else:
-        print("python pdfToTxt.py | errore nell'esecuzione")
+        print("python pdfToTxt.py | execution error")
 except subprocess.CalledProcessError as e:
     print(f"Error occurred while running: {' '.join("python pdfToTxt.py")}")
     print("Return code:", e.returncode)
@@ -16,9 +16,9 @@ except subprocess.CalledProcessError as e:
 try:
     result=subprocess.run(["python", "addVet.py"])
     if(result):
-        print("caricamento su db vettori di embedding | eseguito con successo")
+        print("embedding vectors upload to db | executed successfully")
     else:
-        print("python addVet.py | errore nell'esecuzione")
+        print("python addVet.py | execution error")
 except subprocess.CalledProcessError as e:
     print(f"Error occurred while running: {' '.join("python addVet.py")}")
     print("Return code:", e.returncode)
